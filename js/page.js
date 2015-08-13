07a51eeffb34024eea5e7305b0dbe290b8a17277
Wander Lairson Costa <wcosta@mozilla.com>
2015-08-13 18:29:41 -0300

    Refactor the whole code.
    * Use a different algorithm to detect page loaded point.
    * Plot the graph as responsiveness %.
    * Clear things non useful artifacts.
    * Insert tool tips for configurable values.

--- js/page.js
+++ js/page.js
@@ -1,3 +1,28 @@
+(function() {
+  if (!Array.prototype.findIndex) {
+    Array.prototype.findIndex = function(predicate) {
+      if (this === null) {
+        throw new TypeError('Array.prototype.findIndex called on null or undefined');
+      }
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+      var list = Object(this);
+      var length = list.length >>> 0;
+      var thisArg = arguments[1];
+      var value;
+
+      for (var i = 0; i < length; i++) {
+        value = list[i];
+        if (predicate.call(thisArg, value, i, list)) {
+          return i;
+        }
+      }
+      return -1;
+    };
+  }
+})();
+
 function getParameter(theParameter) {
   "use strict";
   var params = window.location.search.substr(1).split('&');
@@ -12,12 +37,11 @@ function getParameter(theParameter) {
 }
 
 function getCompleteUrl() {
+  "use strict";
   var loc = window.location;
   return loc.protocol + "//" + loc.hostname + loc.pathname
           + "?url="
           + $("#url").val()
-          + "&nsamples="
-          + $("#nsamples").val()
           + "&timeout="
           + $("#timeout").val()
           + "&tolerance="
@@ -34,10 +58,96 @@ function samplesToTimeouts(samples) {
   return timeouts;
 }
 
+function average(myData) {
+  "use strict";
+  return myData.reduce(function(a, b) { return a + b; }) / myData.length;
+}
+
+function findPageLoadedPoint(samples) {
+  var timeouts = samplesToTimeouts(samples);
+  var avg = average(timeouts);
+  var filtered = timeouts.map(function(v) {return v >= avg ? v : 0;});
+  filtered.reverse();
+  return filtered.length - 1 - filtered.findIndex(function(val) {return val != 0;});
+}
+
 function getAverageTimeout(samples, numberOfSamples) {
   "use strict";
   var timeouts = samplesToTimeouts(samples.slice(samples.length - numberOfSamples));
-  return timeouts.reduce(function(a, b) { return a + b; }) / timeouts.length;
+  return average(timeouts);
+}
+
+function processSamples(timeoutValue, samples) {
+  var data = [];
+
+  for (var i = 1; i < samples.length; ++i) {
+    var jankFactor = Math.abs(samples[i] - samples[i-1] - timeoutValue) / timeoutValue;
+    var responsiveness = (1 / (1 + jankFactor)) * 100;
+    data.push({
+      "x": samples[i],
+      "y": responsiveness
+    });
+  }
+
+  var marker = findPageLoadedPoint(samples);
+
+  MG.data_graphic({
+    title: "Page load timings",
+    data: data,
+    width: 1000,
+    height: 600,
+    target: "#plot",
+    min_x: 0,
+    min_y: 0,
+    max_x: samples[samples.length-1],
+    max_y: Math.max.apply(null, data.map(function(v) {return v.y;})) * 1.2,
+    x_accessor: "x",
+    y_accessor: "y",
+    xax_format: function(x) {return parseInt(x);},
+    x_label: "Time (ms)",
+    y_label: "Responsiveness (%)",
+    interpolate: "linear",
+    markers: [{"x": data[marker].x, "label": parseInt(data[marker].x) + "ms"}]
+  });
+}
+
+function getSamples(numberOfLastAvgSamples, timeoutValue, tolerance, callback) {
+  "use strict";
+  var maximumTimeout = timeoutValue * (1 + tolerance/100);
+  var startPoint = performance.now();
+  var samples = [0];
+
+  function timeoutCallback() {
+    samples.push(performance.now() - startPoint);
+    if (samples.length > numberOfLastAvgSamples
+        && getAverageTimeout(samples, numberOfLastAvgSamples+1) <= maximumTimeout) {
+      callback(samples);
+    } else {
+      setTimeout(timeoutCallback, timeoutValue);
+    }
+  }
+
+  setTimeout(timeoutCallback, timeoutValue);
+}
+
+function startPageSampling() {
+  "use strict";
+  var timeoutValue = parseInt($("#timeout").val());
+  var tolerance = parseInt($("#itol").val());
+
+  $("#plot").html("");
+
+  getSamples(
+    20,
+    timeoutValue,
+    tolerance,
+    function(samples) {
+      if (window.opener && getParameter("run")) {
+        window.opener.close();
+      }
+
+      processSamples(timeoutValue, samples);
+    });
 }
 
 function onClick() {
@@ -45,23 +155,18 @@ function onClick() {
     window.open(getCompleteUrl() + "&run=1");
   } else {
     $("#content-frame").attr('src', $("#url").val());
-    getDelaySamples();
+    startPageSampling();
   }
 }
 
 function onLoad() {
   var url = getParameter("url");
-  var nsamples = getParameter("nsamples");
   var timeout = getParameter("timeout");
   var tolerance = getParameter("tolerance");
   var run = getParameter("run");
 
   if (url) {
     $("#url").val(url);
-  }
-
-  if (nsamples) {
-    $("#nsamples").val(nsamples);
   }
 
   if (timeout) {
@@ -75,65 +180,7 @@ function onLoad() {
 
   if (run) {
     window.opener.location.href = $("#url").val();
-    getDelaySamples();
+    startPageSampling();
   }
 }
 
-function getDelaySamples() {
-  "use strict";
-  var N_SAMPLES = parseInt($("#nsamples").val());
-  var TIMEOUT_VALUE = parseInt($("#timeout").val());
-  var TOLERANCE = parseInt($("#itol").val());
-  var MAXIMUM_TIMEOUT = TIMEOUT_VALUE * (1 + TOLERANCE/100);
-  var samples = [];
-
-  $("#plot").html("");
-  $("#link").html("");
-
-  var startPoint = performance.now();
-  samples.push(0);
-
-  var callback = function() {
-    samples.push(performance.now() - startPoint);
-    if (samples.length >= N_SAMPLES && getAverageTimeout(samples, N_SAMPLES) <= MAXIMUM_TIMEOUT) {
-      var marker = samples.length - N_SAMPLES;
-      var timeouts = samplesToTimeouts(samples);
-      var data = [];
-
-      if (window.opener && getParameter("run")) {
-        window.opener.close();
-      }
-
-      samples = samples.slice(1);
-
-      for (var i = 0; i < samples.length; ++i) {
-        data.push({"x": i, "timeout": (timeouts[i] - TIMEOUT_VALUE)});
-      }
-
-      MG.data_graphic({
-        title: "Page load timings",
-        data: data,
-        width: 1000,
-        height: 600,
-        target: "#plot",
-        min_x: 0,
-        max_x: samples.length * 1.2,
-        max_y: Math.max.apply(null, timeouts) * 1.2,
-        x_accessor: "x",
-        y_accessor: "timeout",
-        yax_format: function(v){return v + "ms";},
-        markers: [{"x": marker, "label": samples[marker].toFixed(2) + "ms"}],
-      });
-
-      var link = document.getElementById("link");
-
-      link.href = getCompleteUrl();
-
-      link.innerText = link.href;
-    } else {
-      setTimeout(callback, TIMEOUT_VALUE);
-    }
-  }
-
-  setTimeout(callback, TIMEOUT_VALUE);
-}
